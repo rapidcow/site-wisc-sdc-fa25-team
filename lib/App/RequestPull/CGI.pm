d69a8336999fb6785eb08e7e869a9fd08abc908e
Ethan Meng <devel@rapidcow.org>
2025-10-16 02:10:22 -0500

    try again with Data::Dumper???
    我就不信你是个 SCALAR 了 >:((((  you're not even REMOTELY
    close to the actual payload i sent!!!

--- lib/App/RequestPull/CGI.pm
+++ lib/App/RequestPull/CGI.pm
@@ -80,7 +80,9 @@ sub run
 	unless ($ctx->check_payload($payload)) {
 		return answer($q, '403 Forbidden', '');
 	}
-	return answer($q, '200 OK', "DEBUG: The Payload you sent me was:$CRLF$payload");
+	require Data::Dumper;
+	return answer($q, '200 OK', "DEBUG: The Payload you sent me was:$CRLF" .
+		Data::Dumper->new([$payload])->Terse(1)->Dump);
 	# JSON isn't very type-strict, and under strict ref we
 	# may explode with a runtime error if we are not careful.
 	# So throw these two bad actors out -- all at once...
