83d0812a9b146416db2469f48093a3e40061c9c0
Ethan Meng <devel@rapidcow.org>
2025-10-16 01:48:31 -0500

    wait -- huh????
    wdym malformed 🥀🥀🥀🥀🥀
    
    Invalid JSON: malformed JSON string, neither tag, array, object, number, string or atom, at character offset 0 (before "App::RequestPull::CG...") at /srv/www/lib/site-wisc-sdc-fa25-team/perl5/lib/perl5/App/RequestPull/CGI.pm line 86.

--- lib/App/RequestPull/CGI.pm
+++ lib/App/RequestPull/CGI.pm
@@ -86,7 +86,7 @@ sub run
 	my $json = eval { decode_json($payload) };
 	if ($@ || ref($json) ne 'HASH') {
 		return answer($q, '400 Bad Request',
-			"Invalid JSON: $@$CRLF");
+			"Invalid JSON: $@${CRLF}Payload: $payload$CRLF");
 	}
 	# Don't want to show this even for debugging....
 	delete $json->{hook}->{config}->{secret};
